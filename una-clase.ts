95b5983956e6ac75c41926678291ff0e6179d59f
Marcelo Zapaia <zapaiamarce@gmail.com>
2021-03-08 23:29:58 -0300

    todo

---
+++ una-clase.ts
@@ -0,0 +1,64 @@
+// crear la clase Banda aquí
+class Banda {
+  members: string[];
+  albums: any[];
+  constructor(members: string[], albums: any[]) {
+    this.members = members;
+    this.albums = albums;
+  }
+  getFirstAlbum() {
+    return this.albums[0];
+  }
+  getAllAlbums() {
+    return this.albums;
+  }
+  getAllMembers() {
+    return this.members;
+  }
+  getRandomSong(albumTitle) {
+    const album = this.albums.find((album) => {
+      if (album.title == albumTitle) {
+        return true;
+      }
+    });
+    const posicionRandom = Math.random() * album.songs.length;
+    const posicionRandomRedondeada = Math.floor(posicionRandom);
+    return album.songs[posicionRandomRedondeada];
+  }
+}
+
+function testClaseBanda() {
+  const members = ["dana", "pau"];
+  const fa = {
+    title: "album 1",
+    songs: ["album 1 - tema 1", "album 1 - tema 2"],
+  };
+  const banda = new Banda(members, [
+    fa,
+    {
+      title: "album 2",
+      songs: ["album 2 - tema 1", "album 2 - tema 2", "album 2 - tema 3"],
+    },
+  ]);
+
+  const firstAlbum = banda.getFirstAlbum();
+  const allMembers = banda.getAllMembers();
+  const randomSongAlbum1 = banda.getRandomSong("album 1");
+
+  if (
+    firstAlbum.title == fa.title &&
+    fa.songs.length == firstAlbum.songs.length &&
+    JSON.stringify(allMembers.sort()) == JSON.stringify(members.sort()) &&
+    fa.songs.includes(randomSongAlbum1)
+  ) {
+    console.log("testClaseBanda passed");
+  } else {
+    throw "testClaseBanda not passed";
+  }
+}
+
+function main() {
+  testClaseBanda();
+}
+
+main();
